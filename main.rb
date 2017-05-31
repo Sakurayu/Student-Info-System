34a88445c62803323f56f4fb407a29e1128a36e8
name <name@email.com>
2017-05-30 19:46:18 -0700

    4

--- main.rb
+++ main.rb
@@ -41,3 +41,12 @@ not_found do
   @title = "not found page"
   erb :not_found
 end
+
+configure :development, :test do
+DataMapper.setup(:default,
+"sqlite3://#{Dir.pwd}/development.db")
+end
+configure :production do
+DataMapper.setup(:default,
+ENV['DATABASE_URL'])
+end
